be4e9a5951dcc065175c2a3fd563d6e9bf12c6df
avilca <avilca85@gmail.com>
2025-06-10 13:34:38 -0500

    primeros pasos con operadores

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -10,3 +10,20 @@ multilinea
 
 console.log("Hola, JavaScript")
 console.log('Hola, JavaScript')
+
+
+//tipos de datos
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2) // operador de residuo
+console.log(5 ** 2) // operador de potencia
+
+
+
+
+
