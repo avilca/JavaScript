96655de992ee9b1745f820190e7860c912062527
avilca <avilca85@gmail.com>
2025-06-11 12:07:12 -0500

    variables en JavaScript

---
+++ Basic/01-variables.js
@@ -0,0 +1,25 @@
+// var
+
+var helloWorld = "¡Hello JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+// let variables declaradas dentro de un bloque (asociadas de memorias)
+
+
+let helloWorld2 ="¡Hello JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+
+// const el valor inicial no puede ser reasignado
+
+const helloWorld3 = "¡Hello JavaScript 3!"
+console.log(helloWorld3)
+
+//helloWorld3 = "¡Hola de nuevo, JavaScript 3!"
+//console.log(helloWorld3)
