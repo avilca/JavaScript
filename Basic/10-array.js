854aa6ddd9db3ed583b5afdec243a883c63c8087
avilca <avilca85@gmail.com>
2025-06-26 11:29:41 -0500

    Array

---
+++ Basic/10-array.js
@@ -0,0 +1,49 @@
+// Array
+
+// Declaración
+
+let myArray = []
+let myArray2 = new Array() //sirve para crear un array
+
+console.log(myArray);
+console.log(myArray2);
+
+// Inicialización
+
+myArray = [3]
+myArray2 = new Array(3) 
+
+
+console.log(myArray);
+console.log(myArray2);
+
+myArray = [1, 2, 3, 4]
+myArray2 = new Array(1,2,3,4) 
+
+
+console.log(myArray);
+console.log(myArray2);
+
+
+
+myArray = ["Andres", "Vilca", "Footgolito", 39, true]
+myArray2 = new Array("Andres", "Vilca", "Footgolito", 39, true) 
+
+console.log(myArray);
+console.log(myArray2);
+
+myArray2 = new Array(3)
+myArray2[0] = "Andres"
+myArray2[1] = "Vilca"
+myArray2[2] = "Footgolito"
+
+console.log(myArray2);
+
+myArray = []
+myArray[2] = "Andres"
+myArray[1] = "Vilca"
+
+console.log(myArray);
+
+
+//
