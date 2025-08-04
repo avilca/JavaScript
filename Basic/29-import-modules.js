ea2c4740264805c8c6af22c18dfa220fb7c19f0b
avilca <avilca85@gmail.com>
2025-08-03 19:21:24 -0500

    Importación/exportación de modulos

---
+++ Basic/29-import-modules.js
@@ -0,0 +1,35 @@
+// Importación de módulos
+
+import { add, PI, name, Circle } from "./28-export-modules.js";
+
+import defaultImport from "./28-export-modules.js"; 
+
+// funciones
+
+console.log(add(5, 10));
+
+// Propiedades
+
+console.log(PI);
+console.log(name);
+
+// Clases
+
+let circle = new Circle(10);
+console.log(circle.radius);
+console.log(circle.area().toFixed(2));
+console.log(circle.perimeter().toFixed(2));
+
+
+// Importación por defecto
+
+console.log(defaultImport(5, 10));
+
+//let myClass = new defaultImport()
+//myClass.func()
+
+
+// Proyecto modular
+
+//import { MyClass } from  "./clases/MyClass.js"
+
