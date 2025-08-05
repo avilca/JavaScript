4e7d3a5457fa03a9260d9dcb4fe32740d6e7002b
avilca <avilca85@gmail.com>
2025-08-05 12:40:53 -0500

    Rest, Spread, Closures, Recursividad, funciones parciales, Currying, Callbacks

--- Intermedia/00-avanced-functions.js
+++ Intermedia/00-avanced-functions.js
@@ -59,3 +59,115 @@ handler.arrowGreeting();
     console.log("IIFE con arrow function")
 })();
 
+
+// Parámetros Rest (...)
+
+function sumar(...numbers) {
+
+    let result = 0
+
+    for (let number of numbers) {
+        result += number
+    }
+    return result;
+}
+
+console.log(sumar(1, 2, 3, 4, 5))
+console.log(sumar(10, 15))
+
+
+// Operador Spread (...)
+
+const numbers = [1, 2, 3]
+function sumWithSpread(a, b, c) {
+    return a + b + c 
+}
+
+console.log(sumWithSpread(1, 2, 3)) // Sin Spread
+console.log(sumWithSpread(...numbers)) // con Spread
+
+// Closures (Clausuras)
+
+function createCouter() {
+    let counter = 0
+
+    return function () {
+        counter++
+        console.log(`Contador: ${counter}`)
+    }
+}
+
+
+const counter = createCouter()
+counter()
+counter()
+counter()
+counter()
+counter()
+
+// Recursividad
+
+function factorial(n) {
+    if (n <= 1) {
+        return 1
+    }
+    return n * factorial(n -1)
+}
+
+console.log(factorial(3))
+
+// Funciones parciales
+
+function partialSum(a) {
+    return function (b, c) {
+        return sumar(a, b, c)
+    }
+}
+
+const sumWith = partialSum(4)
+
+console.log(sumWith(2, 3))
+console.log(sumWith(1, 2))
+
+// Currying
+
+function currySum(a) {
+    return function (b) {
+        return function (c) {
+            return function (d) {
+                return sumar(a, b, c, d)
+            }    
+        }
+    }
+}
+
+const sumAB = currySum(1)(2)
+const sumC = sumAB(3)
+
+console.log(sumC(3))
+console.log(sumC(4))
+console.log(sumAB(5)(7))
+
+//  Callbacks
+
+function processData(data, callback) {
+    const result = sumar(...data)
+    callback(result)
+}
+
+
+function processResult(result) {
+    console.log(result)
+}
+
+function processResult2(result) {
+    console.log(`Mi reusltado es:  ${result}`)
+}
+
+
+
+processData([1, 2, 3], processResult)
+processData([1, 2, 3], processResult2)
+processData([1, 2, 3], (result) => {
+    console.log(`Mi resultado en la arrow function es:  ${result}`)
+})
