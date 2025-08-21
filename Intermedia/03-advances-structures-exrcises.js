8af03fb6c1b12a04afff408f2f07a2e4e58a8bd7
avilca <avilca85@gmail.com>
2025-08-21 13:35:08 -0500

    Pregunta 2 resuelta

--- Intermedia/03-advances-structures-exrcises.js
+++ Intermedia/03-advances-structures-exrcises.js
@@ -21,3 +21,39 @@ console.log("======== Recuce =========");
 
 let sumaedades = usuarios.reduce((resedad, age) => resedad + age.age, 0);
 console.log("La suma total de edades es: ", sumaedades);
+
+// 2. Dado un array de números, crea uno nuevo con dichos números elevados al cubo y filtra sólo los números pares
+
+
+console.log("====== Pregunta 2 Elevar al cubo y filtrar pares =========)");
+
+let numeros = [2, 3, 4, 5];
+
+console.log("Array original: ", numeros);
+
+let numerosCuboPares = numeros.map(num => Math.pow(num, 3)).filter(num => num % 2 === 0);
+console.log(numerosCuboPares);
+
+// 3. Utiliza flat y flatMap para crear un ejemplo diferente al de la lección
+
+console.log("====== Pregunta 3 Flat y FlatMap =========)");
+
+// flat:
+console.log("======== Flat =========");
+
+
+let mes = ["Semana 1", ["Semana 2", ["Semana 3","Semana 4"]]];
+console.log("Semanas: ", mes);
+
+let mesOdernado = mes.flat(2);
+console.log("Semanas ordenadas: ", mesOdernado);
+
+// flatMap:
+console.log("======== FlatMap =========");
+
+let arrays = [[2, 4], [6]];
+console.log("Array original: ", arrays); 
+
+
+let flatten = arrays.flatMap(item => item);
+console.log(flatten);
