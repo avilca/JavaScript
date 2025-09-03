6b6306cbd96cd540d5b018a49c310cd4cdbc9758
avilca <avilca85@gmail.com>
2025-09-03 16:17:40 -0500

    Pregunta 6 resuelta

--- Intermedia/08-async-exercises.js
+++ Intermedia/08-async-exercises.js
@@ -151,3 +151,34 @@ async function process() {
 
 process();
 
+
+// 6. Crea una función getUser(id) que devuelva una promesa y simule una llamada a una API (que se demore 2s).
+//    Si el id es menor a 5, la promesa se resuelve con { id, nombre: "Usuario " + id }.
+//    Si el id es 5 o mayor, la promesa se rechaza con el mensaje "Usuario no encontrado".
+//    Usa async/await para llamar a getUser(id) y maneja los errores con try/catch.
+
+
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (id < 5) {
+                resolve({ id, nombre: "Usuario " + id });
+            } else {
+                reject("Usuario no encontrado");
+            }
+        }, 10000);
+    });
+}
+
+async function mostrarUsuario(id) {
+    try {
+        const usuario = await getUser(id);
+        console.log(usuario);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+mostrarUsuario(4); 
+
+
