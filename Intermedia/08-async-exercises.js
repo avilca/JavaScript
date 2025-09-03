f9ecc2ab002fb6cd8807e965c4c71a2472d52fa0
avilca <avilca85@gmail.com>
2025-09-03 12:17:18 -0500

    Pregunta 2 resuelta

--- Intermedia/08-async-exercises.js
+++ Intermedia/08-async-exercises.js
@@ -15,3 +15,41 @@ function mostrarSaludo(mensaje) {
 }
 
 saludar(" Juan", mostrarSaludo);
+
+
+// 2. Crea tres funciones task1(callback), task2(callback) y task3(callback). 
+//    Cada función debe tardar 1 segundo en ejecutarse y luego llamar al callback.
+
+
+console.log("=================== Pregunta 2 ==================");
+
+function task1(callback) {
+    
+    setTimeout(() => {
+        console.log("Tarea 1 completada");
+        callback();
+    }, 1000)
+}
+
+function task2(callback) {
+    setTimeout(() => {
+        console.log("Tarea 2 completada");
+        callback();
+    }, 1000)
+}
+
+function task3(callback) {
+    setTimeout(() => {
+        console.log("Tarea 3 completada");
+        callback();
+    }, 1000)
+}
+
+
+task1(() => {
+    task2(() => {
+        task3(() => {
+            console.log("Todos las tareas completadas");
+        })
+    })
+});
