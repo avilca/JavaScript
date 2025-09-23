20f6d4af1a59acee605ba21812c23c9c73fab997
avilca <avilca85@gmail.com>
2025-09-23 12:21:43 -0500

    Pregunta 2 resuelta

--- Intermedia/16-dom-exercises.js
+++ Intermedia/16-dom-exercises.js
@@ -2,11 +2,13 @@
 
 <h1 id="title">  </h1>
 
-const title = document.getElementById("title")
-title.textContent == "¡Hola Mundo!"
+const title = document.getElementById("title");
+title.textContent == "¡Hola Mundo!";
+
+// 2. Inserta una imagen con id="myImage" y cambia su atributo src a otra URL
+
+<img id="myImage" src="https://google.com"> </img>
 
 
-
-
-
-
+const imagen = document.getElementById("myImage");
+imagen.src = "https://flashscore.com";
