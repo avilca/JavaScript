0ca9a8e7e41e4820df0779338d89a75079079d8f
avilca <avilca85@gmail.com>
2025-09-23 12:06:28 -0500

    Pregunta 1 resuelta

---
+++ Intermedia/16-dom-exercises.js
@@ -0,0 +1,12 @@
+// 1. Crea un elemento (por ejemplo, un <h1 id="title">) y cambia su contenido a "¡Hola Mundo!"" al cargar la página
+
+<h1 id="title">  </h1>
+
+const title = document.getElementById("title")
+title.textContent == "¡Hola Mundo!"
+
+
+
+
+
+
