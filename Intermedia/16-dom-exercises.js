b6a9d86b0c019cac7b4de9ea5de7f637981bf041
avilca <avilca85@gmail.com>
2025-09-23 13:07:14 -0500

    Pregunta 1 resuelta

--- Intermedia/16-dom-exercises.js
+++ Intermedia/16-dom-exercises.js
@@ -1,14 +1,17 @@
 // 1. Crea un elemento (por ejemplo, un <h1 id="title">) y cambia su contenido a "¡Hola Mundo!"" al cargar la página
-
-<h1 id="title">  </h1>
-
 const title = document.getElementById("title");
-title.textContent == "¡Hola Mundo!";
+title.textContent = "¡Hola Mundo!";
 
-// 2. Inserta una imagen con id="myImage" y cambia su atributo src a otra URL
+
+/* // 2. Inserta una imagen con id="myImage" y cambia su atributo src a otra URL
 
 <img id="myImage" src="https://google.com"> </img>
 
 
 const imagen = document.getElementById("myImage");
 imagen.src = "https://flashscore.com";
+
+// 3. Crea un <div id="box"> sin clases y agrega la clase resaltado cuando se cargue la página
+<div  id="box">
+
+</div> */
